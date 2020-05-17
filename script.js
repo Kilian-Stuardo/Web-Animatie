8b4c6797f21b80feb9727332d9efa4566060b2c0
Kilian-Stuardo <45419401+Kilian-Stuardo@users.noreply.github.com>
2020-05-17 14:20:12 +0200

    v1

---
+++ script.js
@@ -0,0 +1,13 @@
+// click color
+var el = document.getElementsByClassName("color");
+for (var i = 0; i < el.length; i++) {
+  el[i].onclick = changeColor;
+}
+
+function changeColor(e) {
+  // get the hex color
+  let hex = e.target.getAttribute("data-hex");
+
+  // set the hex color
+  element.style.fill = hex;
+}
