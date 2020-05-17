f435dd4f0de6e6a6feb769ed856147e63d6f13fe
Kilian-Stuardo <45419401+Kilian-Stuardo@users.noreply.github.com>
2020-05-17 22:02:33 +0200

    v2

--- script.js
+++ script.js
@@ -1,13 +1,36 @@
 // click color
 var el = document.getElementsByClassName("color");
+// All the parents
+var getAllRed = document.querySelector(".red").children;
+var getAllOrange = document.querySelector(".orange").children;
+var getAllYellow = document.querySelector(".yellow").children;
+var getAllGreen = document.querySelector(".green").children;
+var getAllBlack = document.querySelector(".black").children;
+
+
+
+// click event on all the buttons
 for (var i = 0; i < el.length; i++) {
-  el[i].onclick = changeColor;
+  el[i].onclick = changeColor;
 }
 
 function changeColor(e) {
-  // get the hex color
-  let hex = e.target.getAttribute("data-hex");
+  // get the hex color
+  let hex = e.target.getAttribute("data-hex");
+  console.log(hex);
 
-  // set the hex color
-  element.style.fill = hex;
+  // set the hex color
+  changeFills(getAllRed, hex);
+  changeFills(getAllOrange, hex);
+  changeFills(getAllYellow, hex);
+  changeFills(getAllGreen, hex);
+  changeFills(getAllBlack, hex);
+
+}
+
+// function to add the hex.
+function changeFills(variable, hex) {
+  for (var i = 0; i < variable.length; i++) {
+    variable[i].style.fill = hex;
+  }
 }
